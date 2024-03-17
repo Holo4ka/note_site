4a0270dfbcb23042ccce58e94261ba45a239eea0
Игорь <ighor1544@gmail.com>
2024-03-17 13:45:27 +0300

    Initial commit. Login, register, database

---
+++ data/notes.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Note(SqlAlchemyBase):
+    __tablename__ = "notes"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    coords = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    user = orm.relationship('User')
